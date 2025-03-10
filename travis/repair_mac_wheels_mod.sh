a0e48ac462d189883532aaeea2ba3b4808b8e845
Samuel Buchet <samuel.buchet@inrae.fr>
2025-03-10 17:27:53 +0100

    mac test

---
+++ travis/repair_mac_wheels_mod.sh
@@ -0,0 +1,38 @@
+#!\bin/bash
+
+echo "custom wheel repair script" 
+
+wheel_file=$1
+
+echo "wheel file $wheel_file"
+
+mkdir wheel_repair_tmp
+cd wheel_repair_tmp
+unzip $1 -d .
+
+echo "inside wheel: $(ls ./*)"
+
+cd pytoulbar2/.dylibs
+
+libicuuc=`ls libicuuc*.dylib`
+libicui18n=`ls libicui18n*.dylib`
+libicudata=`ls libicudata*.dylib`
+slibicuuc=${libicuuc%.*}
+slibicuuc=${slibicuuc%.*}.dylib
+slibicui18n=${libicui18n%.*}
+slibicui18n=${slibicui18n%.*}.dylib
+slibicudata=${libicudata%.*}
+slibicudata=${slibicudata%.*}.dylib
+
+chmod u+w $libicuuc
+chmod u+w $libicui18n
+
+install_name_tool -change "@loader_path/$slibicudata" "@loader_path/$libicudata" $libicuuc
+install_name_tool -change "@loader_path/$slibicudata" "@loader_path/$libicudata" $libicui18n
+install_name_tool -change "@loader_path/$slibicuuc" "@loader_path/$libicuuc" $libicui18n
+
+rm $wheel_file
+zip -r ./* -O $wheel_file
+cd ..
+rm -rf wheel_repair_tmp
+
