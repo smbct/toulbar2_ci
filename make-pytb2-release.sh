cecb355c2c18745382cbff034c288323cdc578a2
Samuel Buchet <samuel.buchet@inrae.fr>
2025-10-15 15:21:22 +0200

    update script

--- make-pytb2-release.sh
+++ make-pytb2-release.sh
@@ -11,7 +11,7 @@ if output=$(git status --porcelain) && [ -z "$output" ]; then
     sed -i "s/version=.*/version=\"$ver\", # hash $(git rev-parse HEAD) /" setup.py
     git add setup.py
     git commit -m "[pytoulbar2 release] Update pytoulbar2 version for release $ver"
-    git tag -a pytoulbar2$ver -m"$mes"
+    git tag -a pytb2-v$ver -m"$mes"
     git push --no-verify
     git push --tags --no-verify
 else
