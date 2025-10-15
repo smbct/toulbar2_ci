5856d15b091fde2d8c4612ac571d298e582f8313
Samuel Buchet <samuel.buchet@inrae.fr>
2025-10-15 13:48:46 +0200

    new test

--- pytoulbar2/tests/test_pytoulbar2.py
+++ pytoulbar2/tests/test_pytoulbar2.py
@@ -18,3 +18,47 @@ class TestExtension(TestCase):
       res = cfn.Solve()
       assert(res and res[1]==27)
 
+   # this test comes from the tutorial
+   def test_3(self):
+
+      cfn = pytb2.CFN()
+
+      # variables
+      for row in range(9):
+         for col in range(9):
+            cfn.AddVariable('cell_'+str(row)+'_'+str(col), range(9))
+
+      # define known values
+      initial_values = [[5,3,0,0,7,0,0,0,0],
+                     [6,0,0,1,9,5,0,0,0],
+                     [0,9,8,0,0,0,0,6,0],
+                     [8,0,0,0,6,0,0,0,3],
+                     [4,0,0,8,0,3,0,0,1],
+                     [7,0,0,0,2,0,0,0,6],
+                     [0,6,0,0,0,0,2,8,0],
+                     [0,0,0,4,1,9,0,0,5],
+                     [0,0,0,0,8,0,0,7,9]]
+
+      var_index = 0
+      for row in range(9):
+         for col in range(9):
+            if initial_values[row][col] != 0:
+                  cfn.Assign(var_index, initial_values[row][col]-1)
+            var_index += 1
+
+      # row constraints
+      for row_ind in range(9):
+         cfn.AddAllDifferent([row_ind*9+col_ind for col_ind in range(9)])
+
+      # column constraints
+      for col_ind in range(9):
+         cfn.AddAllDifferent([row_ind*9+col_ind for row_ind in range(9)])
+
+      # sub grids constraints
+      for sub_ind1 in range(3): # row offset
+         for sub_ind2 in range(3): # column offset
+            cfn.AddAllDifferent([(sub_ind1*3+row_ind)*9+ sub_ind2*3+col_ind for col_ind in range(3) for row_ind in range(3)])
+                     
+      result = cfn.Solve(showSolutions = 3, allSolutions=1)
+
+
