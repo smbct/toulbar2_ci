fab0fc7d44abe7a9b9cfa5f265b0fcbd35a86e59
Samuel Buchet <samuel.buchet@inrae.fr>
2025-10-15 14:38:24 +0200

    bash script for pytb release

---
+++ make-pytb2-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -n "pytoulbar2 version:"
+read ver
+
+echo -n "Release Message:"
+read mes
+git pull --rebase
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+    sed -i "s/version=.*/version=\"$ver\", # hash $(git rev-parse HEAD) /" setup.py
+    git add setup.py
+    git commit -m "[pytoulbar2 release] Update pytoulbar2 version for release $ver"
+    git tag -a pytoulbar2$ver -m"$mes"
+    git push --no-verify
+    git push --tags --no-verify
+else
+    echo "Git status is not clean. Will not tag!"
+    exit 1
+fi
